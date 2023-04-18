2a5da4b928228ecd1b34daff4d954ba265cfb81d
leo-usa <56208994+leo-usa@users.noreply.github.com>
2023-04-18 11:24:22 -0700

    Update memorybot.py

--- memorybot.py
+++ memorybot.py
@@ -169,7 +169,7 @@ with st.expander("Conversation", expanded=True):
     # Can throw error - requires fix
     download_str = '\n'.join(download_str)
     if download_str:
-        st.download_button('Download',download_str)
+        st.download_button('Download 下载',download_str)
 
 # Display stored conversation sessions in the sidebar
 for i, sublist in enumerate(st.session_state.stored_session):
